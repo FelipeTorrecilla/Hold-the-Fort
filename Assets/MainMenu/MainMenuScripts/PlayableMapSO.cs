3838c61457fcaef258e0ed6ddfd83968224f7e24
Floppy <felipe.torrecillarp@gmail.com>
2023-06-05 16:29:58 -0300

    Version 1 MainMenu. Functional, can Start Game. Added map select. Missing Settings and credits

---
+++ Assets/MainMenu/MainMenuScripts/PlayableMapSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayableMapSO : ScriptableObject
+{
+    public string title;
+    public Image mapImage;
+}
